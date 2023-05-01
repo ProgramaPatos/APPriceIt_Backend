d462b40ce795f3422c10da8eb4586b12d9a1fd55
sechmo <sesanchezmo@unal.edu.co>
2023-05-01 14:45:39 -0500

    :auth Fixed refresh return type

--- src/auth/services/auth/auth.service.ts
+++ src/auth/services/auth/auth.service.ts
@@ -4,6 +4,7 @@ import { JwtService } from '@nestjs/jwt';
 import { classToPlain, instanceToPlain, plainToClass, plainToInstance } from 'class-transformer';
 import { validate } from 'class-validator';
 import RefreshRequestDTO from 'src/auth/dtos/refresh-request.dto';
+import RefreshResponseDTO from 'src/auth/dtos/refresh-response.dto';
 import SignInRequestDTO from 'src/auth/dtos/signin-request.dto';
 import SignInResponseDTO from 'src/auth/dtos/signin-response.dto';
 import TokenPayloadDTO from 'src/auth/dtos/token-payload.dto';
@@ -30,13 +31,11 @@ export class AuthService {
         return tokens;
     }
 
-    async refresh({ refreshToken }: RefreshRequestDTO) {
+    async refresh({ refreshToken }: RefreshRequestDTO): Promise<RefreshResponseDTO> {
         try {
             const payloadRaw = this.jwtService.verify(refreshToken);
-            console.log("raw", payloadRaw);
             const payload = plainToInstance(TokenPayloadDTO, payloadRaw, { excludeExtraneousValues: true });
 
-            console.log("pay", payload);
             await validate(payload);
             if (await this.usersService.getRefreshToken(payload.userEmail) !== refreshToken) {
                 throw new Error('Invalid token');
